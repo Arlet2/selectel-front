522684b2ec9b9bdbaf6b8e92ed2367beceb4718e
LeshaInc <includeurl@gmail.com>
2024-02-23 19:36:02 +0300

    Add search modal

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,9 +1,71 @@
+"use client";
+
+import { useState } from "react";
 import styles from './styles.module.css';
 import cn from 'classnames';
 
+const {
+    CitySelect,
+    CountrySelect,
+    StateSelect,
+} = require('react-country-state-city');
+
+const COUNTRY_ID = 182;
+
+function Modal() {
+    const [ state, setState ] = useState();
+
+    return (
+        <form className={styles.modal}>
+            <h1>Данные для поиска</h1>
+            <div className={styles.inputContainer}>
+                <label className={styles.label}>Тип животного</label>
+                <select className="input">
+                    <option>Кошка</option>
+                    <option>Собака</option>
+                </select>
+            </div>
+            <div className={styles.inputContainer}>
+                <label className={styles.label}>Группа крови</label>
+                <select className="input">
+                    <option>1+</option>
+                    <option>2-</option>
+                </select>
+            </div>
+            <div className={styles.inputContainer}>
+                <label className={styles.label}>Город</label>
+                <select className="input">
+                    <option>Санкт-Петербург</option>
+                    <option>Москва</option>
+                </select>
+            </div>
+            <div className={styles.inputContainer}>
+                <label className={styles.label}>Район</label>
+                <select className="input">
+                    <option>Шушары</option>
+                    <option>Нижний Тагил</option>
+                </select>
+            </div>
+            <div className={styles.inputContainer}>
+                <label className={styles.label}>Количество крови (мл)</label>
+                <input type="number" className="input" placeholder="0"/>
+            </div>
+            <div className={styles.inputContainer}>
+                <label className={styles.label}>Дата окончания поиска</label>
+                <input required type="date" className="input"/>
+            </div>
+            <button className={cn("buttonLogin", styles.modalButton)} type='submit'>Создать заявку</button>
+        </form>
+    )
+}
+
 export default function Page() {
+    const [ modalVisible, setModalVisible ] = useState(false)
+    
     return (
         <div className={styles.container}>
+            { modalVisible && <div className={styles.darkness} onClick={() => setModalVisible(false)}/> }
+            { modalVisible && <Modal/> }
             <div className={styles.textContainer}>
                 <h1 className='header'>Поиск донора</h1>
                 <div className='divider'></div>
@@ -28,7 +90,7 @@ export default function Page() {
                 </p>
             </div>  
             <div className={styles.buttonContainer}>
-                <button className={cn('button', styles.button)}>Cоздать заявку</button>
+                <button className={cn('button', styles.button)} onClick={() => setModalVisible(true)}>Cоздать заявку</button>
             </div>
         </div>
     )
