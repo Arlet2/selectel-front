63eb3828c0e11a03ae1f25087da87d1b1d5243fe
LeshaInc <includeurl@gmail.com>
2024-02-23 13:19:49 +0300

    Try to fix build

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    output: 'standalone',
     assetPrefix: isProd ? undefined : './',
 };
 
