f92238b8f6351f463ca2937a17cfd55b3f776ffd
nastyabeggin <45201186+nastyabeggin@users.noreply.github.com>
2024-02-23 20:55:28 +0300

    Add 404 error page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,17 @@
 import Link from 'next/link'
 import styles from './page.module.css';
 
-/* страница 404 */
- 
 export default function NotFound(){
-  return (<>
-    <title>Страница не найдена - petdonor.ru</title>
-    <div className={styles.notfound}>
+  return (
+    <>
+      <title>Страница не найдена - petdonor.ru</title>
+      <div className={styles.notfound}>
       <h2 className="header">Не найдено</h2>
       <p>Котик потерялся?</p>
-      <Link href="/" className="lingBlue">Вернуть котика домой</Link>
-    </div>
-  </>)
+        <Link href="/">
+          <a className="linkBlue">Вернуть котика домой</a> {}
+        </Link>
+      </div>
+    </>
+  )
 }
