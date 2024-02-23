99fd0b7c23fd039ed637b566bd9ace70298d87bc
dasha <dasha@skv.name>
2024-02-23 14:42:15 +0300

    added header

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,9 +1,30 @@
+import Image from 'next/image';
+import donorSearchLogo from '@icons/donorSearch.svg';
+import selectelLogo from '@icons/selectel.svg'
+import geoIcon from '@icons/geo.svg';
 import styles from './styles.module.css';
+import Link from 'next/link';
+
+// TODO: прикрутить выбор локации, переход на страницу входа
 
 export const Header = () => {
     return (
         <div className={styles.container}>
-            it is header
+            <div className={styles.logoContainer}>
+                <Image src={donorSearchLogo} alt='Logo Selectel'/>
+                <Image src={selectelLogo} alt='Logo Selectel'/>
+            </div>
+            <div className={styles.navContainer}>
+                <Link className={styles.headerLink} href='/about'>О сайте</Link>
+                <Link className={styles.headerLink} href='/search'>Поиск донора</Link>
+            </div>
+            <div className={styles.infoContainer}>
+                <div className={styles.geolocation}>
+                    <Image className={styles.icon} src={geoIcon} alt='Geo location icon'/>
+                    Все регионы
+                </div>
+                <button className={styles.buttonLogin}>Войти</button>
+            </div>
         </div>
     )
 }
