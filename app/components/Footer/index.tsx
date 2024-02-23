959887dd5b5742e74fd2b12fbb12f4577cf3d802
nastyabeggin <45201186+nastyabeggin@users.noreply.github.com>
2024-02-23 20:44:42 +0300

    Add gif into footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,9 +1,12 @@
 import styles from './styles.module.css';
+import gifFooter from '../../../public/gifs/dog.gif';
+import Image from 'next/image';
 
 export const Footer = () => {
     return (
         <div className={styles.container}>
-            Добряки © 2024 <br /> Сделано для Selectel Hack 2024
+                Добряки © 2024 <br /> Сделано для Selectel Hack 2024
+            <Image className={styles.footerGif} src={gifFooter} alt="GIF description" layout="fixed" />
         </div>
     )
 }
