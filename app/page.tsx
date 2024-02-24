eb9593b840a6ed82ca1b850a203efcb6062f60ef
nastyabeggin <45201186+nastyabeggin@users.noreply.github.com>
2024-02-24 19:25:37 +0300

    Beautify / page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import { useDispatch, useSelector } from "react-redux";
 import styles from "./page.module.css";
-import { Skeleton } from "@components/Skeleton";
+import cn from 'classnames';
 
 /* главная страница, которая будет открываться по / */
 
@@ -14,30 +13,46 @@ export default function Home() {
         <div className={styles.textContainer}>
             <h1 className='header'>О проекте</h1>
             <div className='divider'></div>
-            <p className={styles.text}>
-            Наш сервис представляет собой уникальную платформу, призванную <strong>спасать жизни наших четвероногих друзей</strong> , предоставляя возможность оперативно находить доноров крови для домашних животных. Разработанная с любовью и заботой, наша платформа становится мостом между владельцами питомцев, которым требуется помощь, и добровольцами, готовыми поделиться жизненно важным ресурсом своих любимцев.            </p>
-            <h2 className='subtitle'>Как это работает?</h2>
-            <div className={styles.listContainer}>
-              <p className={styles.text}>
-                  <strong>1. Регистрация: </strong> Простая регистрация через соцсети или электронную почту. Укажите основные данные и настройте приватность контактов.
-              </p>
-              <p className={styles.text}>
-              <strong> 2. Профиль питомца: </strong> Создайте карточку для вашего животного с важной информацией: вид, порода, группа крови и прививки.
-              </p>
-              <p className={styles.text}>
-              <strong>3. Поиск донора: </strong> Подайте запрос на поиск донора, указав необходимые детали: причина, место, объем крови и срок.            
-              </p>
-              <p className={styles.text}>
-              <strong>4. Фильтры поиска: </strong> Используйте удобные фильтры для поиска актуальных запросов на донорство по городу и типу животного
-              </p>
+            <p className={styles.textDefault}>
+            Наш сервис представляет собой уникальную платформу, призванную <strong>спасать жизни наших четвероногих друзей</strong> , предоставляя возможность оперативно находить доноров крови для домашних животных.</p>
+            <h2 className={styles.subtitle}>Как это работает?</h2>
+            <div className={styles.allStepsContainer}>
+              <div className={styles.stepContainerLower}>
+                <div className={styles.blueNumber}>1</div>
+                <div className={styles.content}>
+                  <p className={styles.description}>Простая регистрация через соцсети или электронную почту. Укажите основные данные и настройте приватность контактов</p>
+                  <h2 className={styles.blueTitle}>регистрация</h2>
+                </div>
+              </div>
+              <div className={styles.stepContainerUpper}>
+                <div className={styles.pinkNumber}>2</div>
+                <div className={styles.content}>
+                <h2 className={styles.pinkTitle}>профиль питомца</h2>
+                  <p className={styles.description}>Создайте карточку для вашего животного с важной информацией: вид, порода, группа крови и прививки</p>
+                </div>
+              </div>
+              <div className={styles.stepContainerUpper}>
+                <div className={styles.content}>
+                <h2 className={cn(styles.pinkTitle, styles.rightAlign)}>поиск донора</h2>
+                  <p className={cn(styles.description, styles.selfRightAlign, styles.rightAlign)}>Подайте запрос на поиск донора, указав необходимые детали: причина, место, объем крови и срок</p>
+                </div>
+                <div className={styles.pinkNumber}>3</div>
+              </div>
+              <div className={styles.stepContainerLower}>
+                <div className={styles.content}>
+                  <p className={cn(styles.description, styles.selfRightAlign, styles.rightAlign)}>Подайте запрос на поиск донора, указав необходимые детали: причина, место, объем крови и срок</p>
+                  <h2 className={cn(styles.blueTitle, styles.rightAlign)}>поиск донора</h2>
+                </div>
+                <div className={styles.blueNumber}>4</div>
+              </div>
             </div>
-            <h2 className='subtitle'>
+            <h2 className={styles.subtitle}>
               Для чего этот сервис?
             </h2>
-            <p className={styles.text}>
+            <p className={styles.textDefault}>
             Наш сервис создан для упрощения поиска доноров крови для домашних животных, обеспечивая быструю помощь в критических ситуациях и спасая жизни питомцев.             </p>
-            <p className={styles.text}>
-            <strong> Мы стремимся к созданию сильного сообщества взаимопомощи, где каждый может внести свой вклад.  </strong>
+            <p className={styles.textAccent}>
+            <strong> Мы стремимся к созданию сильного сообщества взаимопомощи, где каждый может внести свой вклад!</strong>
             </p>
         </div>  
     </div>
